650583f005048a046da2281464a85a253ee2fb77
David B. Waters <david@davidbwaters.net>
2018-08-04 03:47:13 -0400

    added babel and terser minifier to rollup config, transpiled and minified output

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 
 // Rollup Config
 
+import resolve from "rollup-plugin-node-resolve"
+import babel from "rollup-plugin-babel"
+import { terser }  from "rollup-plugin-terser"
+
 export default {
   input: "src/substrings.js",
   output: [
@@ -12,5 +16,12 @@ export default {
       file: "dist/substrings.mjs",
       format: "es"
     }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: "node_modules/**"
+    }),
+    terser()
   ]
 }
